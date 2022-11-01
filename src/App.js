99fd7b697c17e195dce7cce5916cd7801e34f3c2
Raad <ashabulraad@gmail.com>
2022-11-02 05:53:24 +0600

    Routes fix

--- src/App.js
+++ src/App.js
@@ -1,49 +1,8 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
-import Home from './Components/Home/Home';
-import DoctorLogin from './Components/Login/DoctorLogin/DoctorLogin';
-import Login from './Components/Login/Login/Login';
-import PatientLogin from './Components/Login/PatientLogin/PatientLogin';
-import DoctorSignup from './Components/SignUp/DoctorSignup/DoctorSignup';
-import PatientSignup from './Components/SignUp/PatientSignup/PatientSignup';
-import SignUp from './Components/SignUp/SignUp/SignUp';
+import { router } from './Routes/Routes';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Home></Home>
-    },
-    {
-      path: '/home',
-      element: <Home></Home>
-    },
-    {
-      path: '/login',
-      element: <Login></Login>
-    },
-    {
-      path: '/signup',
-      element: <SignUp></SignUp>
-    },
-    {
-      path: '/patient-login',
-      element: <PatientLogin></PatientLogin>
-    },
-    {
-      path: '/doctor-login',
-      element: <DoctorLogin></DoctorLogin>
-    },
-    {
-      path: '/patient-signup',
-      element: <PatientSignup></PatientSignup>
-    },
-    {
-      path: '/doctor-signup',
-      element: <DoctorSignup></DoctorSignup>
-    }
-  ]);
-
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
---
+++ src/Routes/Routes.js
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../Components/Home/Home';
+import DoctorLogin from '../Components/Login/DoctorLogin/DoctorLogin';
+import Login from '../Components/Login/Login/Login';
+import PatientLogin from '../Components/Login/PatientLogin/PatientLogin';
+import DoctorSignup from '../Components/SignUp/DoctorSignup/DoctorSignup';
+import PatientSignup from '../Components/SignUp/PatientSignup/PatientSignup';
+import SignUp from '../Components/SignUp/SignUp/SignUp';
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home></Home>
+    },
+    {
+        path: '/home',
+        element: <Home></Home>
+    },
+    {
+        path: '/login',
+        element: <Login></Login>
+    },
+    {
+        path: '/signup',
+        element: <SignUp></SignUp>
+    },
+    {
+        path: '/patient-login',
+        element: <PatientLogin></PatientLogin>
+    },
+    {
+        path: '/doctor-login',
+        element: <DoctorLogin></DoctorLogin>
+    },
+    {
+        path: '/patient-signup',
+        element: <PatientSignup></PatientSignup>
+    },
+    {
+        path: '/doctor-signup',
+        element: <DoctorSignup></DoctorSignup>
+    }
+]);
